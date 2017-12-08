fa79dfc1f4152b5ae999ca4ae9db7a7be85196dd
熊宇成 <13590461995@163.com>
2017-12-08 14:41:24 +0800

    后端代理获取歌曲列表&Scroll组件

--- src/components/recommend/recommend.vue
+++ src/components/recommend/recommend.vue
@@ -1,26 +1,41 @@
 <template>
   <div class="recommend">
-    <div class="recommend-content">
-      <div v-if="recommend.sliders.length" class="slider-wrapper">
-        <slider>
-          <div v-for="item in recommend.sliders" :key="item.id">
-            <a :href="item.linkUrl">
-              <img :src="item.picUrl" alt="">
-            </a>
-          </div>
-        </slider>
+    <scroll ref="scroll" class="recommend-content" :data="recommend.discList">
+      <div>
+        <!-- slider -->
+        <div v-if="recommend.sliders.length" class="slider-wrapper">
+          <slider>
+            <div v-for="item in recommend.sliders" :key="item.id">
+              <a :href="item.linkUrl">
+                <img @load="loadImage" :src="item.picUrl">
+              </a>
+            </div>
+          </slider>
+        </div>
+        <!-- songList -->
+        <div class="recommend-list">
+          <h1 class="list-title">热门歌单推荐</h1>
+          <ul>
+            <li v-for="item in recommend.discList" :key="item.id" class="item">
+              <div class="icon">
+                <img width="60" height="60" v-lazy="item.imgurl">
+              </div>
+              <div class="text">
+                <div class="name" v-html="item.creator.name"></div>
+                <div class="desc" v-html="item.dissname"></div>
+              </div>
+            </li>
+          </ul>
+        </div>
       </div>
-      <div class="recommend-list">
-        <h1 class="list-title">热门歌单推荐</h1>
-        <ul></ul>
-      </div>
-    </div>
+    </scroll>
   </div>
 </template>
 
 <script>
 import Slider from 'base/slider/slider'
-import { getRecommend } from 'api/recommend'
+import Scroll from 'base/scroll/scroll'
+import { getRecommend, getDiscList } from 'api/recommend'
 import { ERR_OK } from 'api/config'
 
 export default {
@@ -28,25 +43,45 @@ export default {
   data() {
     return {
       recommend: {
-        sliders: []
+        sliders: [],
+        discList: []
       }
     }
   },
   created() {
-    this._getRecommend()
+    setTimeout(() => {
+      this._getRecommend()
+    }, 2000)
+    this._getDiscList()
   },
   methods: {
     _getRecommend() {
       getRecommend().then((res) => {
         if (res.code === ERR_OK) {
-          console.log(res.data.slider)
+          // console.log(res.data.slider)
           this.recommend.sliders = res.data.slider
         }
       })
+    },
+    _getDiscList() {
+      getDiscList().then((res) => {
+        if (res.code === ERR_OK) {
+          console.log(res.data.list)
+          this.recommend.discList = res.data.list
+        }
+      })
+    },
+    // slider高度是由图片获取到后撑开的 checkLoaded用于节流
+    loadImage() {
+      if (!this.checkLoaded) {
+        this.$refs.scroll.refresh()
+        this.checkLoaded = true
+      }
     }
   },
   components: {
-    Slider
+    Slider,
+    Scroll
   }
 }
 </script>
--- src/main.js
+++ src/main.js
@@ -3,8 +3,14 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 import fastclick from 'fastclick'
+import VueLazyload from 'vue-lazyload'
 
 import 'common/stylus/index.styl'
+
+Vue.use(VueLazyload, {
+  loading: require('common/images/default.png'),
+  attempt: 2
+})
 
 fastclick.attach(document.body)
 Vue.config.productionTip = false
