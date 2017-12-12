a6c1612305d66f528b7dc06a3ec4ab2ca4750460
熊宇成 <13590461995@163.com>
2017-12-12 15:32:24 +0800

    歌手详情子路由配置&vuex配置

---
+++ src/components/singer-detail/singer-detail.vue
@@ -0,0 +1,28 @@
+<template>
+  <transition name="slide">
+    <div class="singer-detail"></div>
+  </transition>
+</template>
+
+<script>
+export default {
+  name: 'singer-detail'
+}
+</script>
+
+<style scoped lang="stylus">
+.singer-detail
+  position: fixed;
+  left: 0;
+  right: 0;
+  top: 0;
+  bottom: 0;
+  z-index: 100;
+  background-color: #58a;
+
+.slide-enter-active, .slide-leave-active
+  transition: all 0.3s;
+.slide-enter, .slide-leave-to
+  transform: translate3d(100%, 0, 0);
+</style>
+
---
+++ src/store/index.js
@@ -0,0 +1,24 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+
+import * as actions from './actions'
+import * as getters from './getters'
+import state from './state'
+import mutations from './mutations'
+
+// 修改state时在console打印
+import createLogger from 'vuex/dist/logger'
+
+Vue.use(Vuex)
+
+const debug = process.env.NODE_ENV !== 'production'
+
+export default new Vuex.Store({
+  actions,
+  getters,
+  state,
+  mutations,
+  // 严格模式，非法修改state时报错
+  strict: debug,
+  plugins: debug ? [createLogger()] : []
+})
